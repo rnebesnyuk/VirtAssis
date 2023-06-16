2fe6af646f16ac3c64243cd3fb16d68b7e6e2bb9
rnebesnyuk <98305190+rnebesnyuk@users.noreply.github.com>
2023-06-16 09:19:58 +0300

    Delete additionalapp directory

--- additionalapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdditionalappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'additionalapp'
--- additionalapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .scraping import news_rss
-from .weather import read_weather
-
-
-def main(request):
-    weather = read_weather()
-    news = news_rss()
-    return render(request, 'additional_app/index.html', {'weather': weather, 'news': news})
